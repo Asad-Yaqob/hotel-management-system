278ac44bb89221f16629766717b798db6df1d205
AsadYaqoob <asadmuhammad09123@gmail.com>
2025-01-05 10:51:18 +0500

    fix: update COOKIE_OPTIONS to include SameSite and MaxAge for enhanced security

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,6 +1,8 @@
 export const DBNAME = "hotelmanagementsystem";
 
 export const COOKIE_OPTIONS = {
-  httpOnly: true,
-  secure: true,
+  HttpOnly: true, 
+  Secure: true, 
+  SameSite: "Strict", 
+  MaxAge: 3600,
 };
