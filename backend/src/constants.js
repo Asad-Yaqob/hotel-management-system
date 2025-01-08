994a6e83177fb9014a4362cbd81bfb65e369ed54
AsadYaqoob <asadmuhammad09123@gmail.com>
2025-01-08 10:35:02 +0500

    fix: add SameSite and Path options to COOKIE_OPTIONS for improved cookie handling

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,6 +1,8 @@
 export const DBNAME = "hotelmanagementsystem";
 
 export const COOKIE_OPTIONS = {
-  HttpOnly: true, 
-  Secure: true, 
+  HttpOnly: true,
+  Secure: true,
+  SameSite: "None", 
+  Path: "/", 
 };
